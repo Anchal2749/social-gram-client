ef3b92564afe70ebc41b24e6b10fd26fa2eab374
Chandresh Prajapati <chandreshprajapaticppc@gmail.com>
2025-06-16 19:33:08 +0530

    updated backend uri

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000"
-// export const BASE_URL="https://social-gram-backend-ovlj.onrender.com"
+// export const BASE_URL = "http://localhost:8000"
+export const BASE_URL="https://social-gram-backend-1.onrender.com"
